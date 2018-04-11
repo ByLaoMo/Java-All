0e035f355ac444dcb85e6907dba545b0f316258d
LaoMo <root@laomo.io>
2018-04-11 15:41:14 +0800

    文档初始化

--- src/java_service/log4j/Log4J_00.java
+++ src/java_service/log4j/Log4J_00.java
@@ -3,6 +3,7 @@ package java_service.log4j;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class Log4J_00 {
 
@@ -11,7 +12,11 @@ public class Log4J_00 {
 
     public static void main(String[] args) {
 
-        BasicConfigurator.configure();
+        /* 运用默认配置 */
+        // BasicConfigurator.configure();
+
+        /* 运用自定义配置 */
+        PropertyConfigurator.configure("D:\\20_Code\\02_IDEA\\01\\Java-All\\src\\java_service\\log4j\\log4j.porperties");
         logger.setLevel(Level.ALL);
 
         logger.trace("跟踪信息");           // 最低等级日志输出
